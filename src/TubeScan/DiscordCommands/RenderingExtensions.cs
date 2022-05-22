ebb15c9d9952d02b33275f63c845bb89ec583b82
tonycknight <tonycknight@users.noreply.github.com>
2022-05-22 16:48:08 +0100

    less chance of failures...

--- src/TubeScan/DiscordCommands/RenderingExtensions.cs
+++ src/TubeScan/DiscordCommands/RenderingExtensions.cs
@@ -32,14 +32,16 @@ namespace TubeScan.DiscordCommands
                         .Build();
         }
 
-        public static string RenderLinesStatus(this IList<Models.Line> lines, Dictionary<string, Models.LineStatus> stats, bool fullDetails)
-            => lines.OrderBy(l => l.Name)
-                .SelectMany(line =>
-                {
-                    var stat = stats.GetOrDefault(line.Id);
+        public static string RenderLinesStatus(this IEnumerable<Models.Line> lines, Dictionary<string, Models.LineStatus> stats, bool fullDetails)
+            => lines.Where(l => l != null)
+                    .OrderBy(l => l.Name)
+                    .SelectMany(line =>
+                    {
+                        var stat = stats.GetOrDefault(line.Id);
 
-                    return GetLineStatus(line, stat, fullDetails);
-                }).Join(Environment.NewLine);
+                        return GetLineStatus(line, stat, fullDetails);
+                    })
+                    .Join(Environment.NewLine);
 
         private static IEnumerable<string> GetCrowdingLines(this Models.StationStatus status)
         {
