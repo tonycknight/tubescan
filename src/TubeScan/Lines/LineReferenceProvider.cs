12d7143c0238cfe884fd8b74f243cc6b78ea3e9a
tonycknight <tonycknight@users.noreply.github.com>
2022-05-31 21:40:13 +0100

    refactoring of line provision

--- src/TubeScan/Lines/LineReferenceProvider.cs
+++ src/TubeScan/Lines/LineReferenceProvider.cs
@@ -2,26 +2,26 @@
 
 namespace TubeScan.Lines
 {
-    internal class LineReferenceProvider
+    internal class LineReferenceProvider : ILineReferenceProvider
     {
         private readonly IList<Line> _lines = CreateLines();
 
-        public IEnumerable<Line> GetLines() => _lines;
+        public IList<Line> GetLines() => _lines;
 
         private static IList<Line> CreateLines() 
             => new Line[]
-            {
-                new Line("piccadilly", "Piccadilly", "#0019A8"),
-                new Line("bakerloo", "Bakerloo", "#B26313"),
-                new Line("central", "Central", "#DC241F"),
-                new Line("circle", "Circle", "#FFD329"),
-                new Line("district", "District", "#007D32"),
-                new Line("hammersmith-city", "Hammersmith & City", "#F4A9BE"),
-                new Line("jubilee", "Jubilee", "#A1A5A7"),
-                new Line("metropolitan", "Metropolitan", "#9B0058"),
-                new Line("northern", "Northern", "#000000"),
-                new Line("victoria", "Victoria", "#0098D8"),
-                new Line("waterloo-city", "Waterloo & City", "#93CEBA")
-            };
+                {
+                    new Line("piccadilly", "Piccadilly", "#0019A8"),
+                    new Line("bakerloo", "Bakerloo", "#B26313"),
+                    new Line("central", "Central", "#DC241F"),
+                    new Line("circle", "Circle", "#FFD329"),
+                    new Line("district", "District", "#007D32"),
+                    new Line("hammersmith-city", "Hammersmith & City", "#F4A9BE"),
+                    new Line("jubilee", "Jubilee", "#A1A5A7"),
+                    new Line("metropolitan", "Metropolitan", "#9B0058"),
+                    new Line("northern", "Northern", "#000000"),
+                    new Line("victoria", "Victoria", "#0098D8"),
+                    new Line("waterloo-city", "Waterloo & City", "#93CEBA")
+                };
     }
 }
