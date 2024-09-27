71985cc8cd150ef12c786059e005294bb7935b46
tonycknight <tonycknight@users.noreply.github.com>
2024-09-27 17:46:39 +0100

    allow redirects; set message intents

--- src/TubeScan/ProgramBootstrap.cs
+++ src/TubeScan/ProgramBootstrap.cs
@@ -39,9 +39,13 @@ namespace TubeScan
                 .AddSingleton<Stations.StationCacheRefreshJob>();
 
             var rateLimit = Policy.RateLimitAsync<HttpResponseMessage>(500, TimeSpan.FromMinutes(1), 20);
-
-            var hcb = col.AddHttpClient(Tfl.ITflClient.HttpClientName)
-                         .AddPolicyHandler(rateLimit);
+            
+            var hcb = col.AddHttpClient(Tfl.ITflClient.HttpClientName).ConfigurePrimaryHttpMessageHandler(() =>
+                new HttpClientHandler()
+                {
+                    AllowAutoRedirect = true,
+                    AutomaticDecompression = System.Net.DecompressionMethods.GZip | System.Net.DecompressionMethods.Deflate
+                }).AddPolicyHandler(rateLimit);
 
             return col.BuildServiceProvider();
         }
